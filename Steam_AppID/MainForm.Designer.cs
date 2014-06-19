fbfcb1d4208ca56160e503d4eb0a9b31abae76e0
maio290 <maio290@maio290.de>
2014-06-20 01:50:37 +0200

    First release

---
+++ Steam_AppID/MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mario
+ * Date: 20.06.2014
+ * Time: 00:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Steam_AppID
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(150, 78);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(76, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(33, 78);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(94, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Target App-ID";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(18, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(230, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Select Folder (must contain a steam_appid.txt)";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(18, 35);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(230, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Browse";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(18, 141);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(230, 36);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Patch";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(260, 189);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Steam App-ID Changer";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		
+		
+		
+		void Label1Click(object sender, System.EventArgs e)
+		{
+			
+		}
+	}
+}
---
+++ Steam_AppID/MainForm.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mario
+ * Date: 20.06.2014
+ * Time: 00:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Steam_AppID
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		string filename = @"\steam_appid.txt";
+		string folder =@"";
+
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+		System.Windows.Forms.FolderBrowserDialog objDialog = new FolderBrowserDialog();
+		objDialog.Description = "Description";
+		objDialog.SelectedPath=@"C:\";    
+		DialogResult objResult = objDialog.ShowDialog(this);
+		if (objResult == DialogResult.OK)
+ 		folder = objDialog.SelectedPath;
+		
+		else
+		;
+	
+		}
+		void Button2Click(object sender, System.EventArgs e)
+		{
+				
+			
+			if(File.Exists(folder + filename))
+			 {
+			        File.Delete(folder + filename);
+			        FileStream appid = new FileStream(folder + filename,FileMode.OpenOrCreate, FileAccess.Write);
+			        StreamWriter writer = new StreamWriter(appid);
+			        writer.Write(textBox1.Text);
+			        writer.Close();
+			        MessageBox.Show("Successfully patched", "Steam App-ID Changer", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+			        
+			 }
+			else
+			{
+MessageBox.Show("Patching unsuccessfull", "Steam App-ID Changer", 
+    MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
